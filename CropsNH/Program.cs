72113812b1993cf5b0055b527a4ebbfeaf171ed5
BlueWeabo <ilia.iliev2005@gmail.com>
2023-02-20 15:45:32 +0200

    a

--- CropsNH/Program.cs
+++ CropsNH/Program.cs
@@ -6,13 +6,13 @@ namespace CropsNH
     internal static class Program
     {
         public static MySqlConnection mConnection = new(@"server=localhost;uid=root;pwd=1234;database=CropsNH");
-        public static MainMenu? mMenu;
+        public static MainMenu mMenu = new();
         [STAThread]
         static void Main()
         {
             mConnection.Open();
             ApplicationConfiguration.Initialize();
-            Application.Run(mMenu = new MainMenu());
+            Application.Run(mMenu);
         }
     }
 }
